d177dc0868199eeb505faaf1ab096295028a1c96
hoseasandstrom <Lakyn4ever>
2016-10-03 15:33:25 -0400

    Initial Set up

---
+++ src/main/java/com/example/ParkCharlestonApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkCharlestonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkCharlestonApplication.class, args);
+	}
+}
